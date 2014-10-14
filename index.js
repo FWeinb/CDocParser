9f54d2cf89907d166f3fd0ba09c384c871022dbb
Josh Johnston <josh@x-team.com>
2014-10-14 21:24:29 +1100

    removed matchIndex

--- index.js
+++ index.js
@@ -117,7 +117,6 @@ var CommentExtractor = (function () {
       comments.push({
         lines: lines,
         type: commentType,
-        matchIndex: matchIndex,
         lineNumberRange: [startLineNumber, endLineNumber],
         context: this.parseContext(code.substr(matchIndex), lineNumberWithOffsetFor)
       });
